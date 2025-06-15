ed89f4c68517e2d88dbaf3efef5daf1064282cbe
NazmiHakim <145305124+NazmiHakim@users.noreply.github.com>
2025-06-15 23:40:22 +0800

    Refactor Hilt plugin configuration and theme status bar color
    The Hilt Android Gradle plugin declaration has been moved from the top-level `plugins` block to the `buildscript` block in the project-level `build.gradle.kts` file. This is the recommended way to apply classpath dependencies for Gradle plugins.
    
    In `Theme.kt`, the status bar and navigation bar color assignments were updated to use `also` for a more concise expression. A `@file:Suppress("DEPRECATION")` annotation was also added.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,3 @@
-// In Jvent/build.gradle.kts
-
-// ADD THIS BUILDSCRIPT BLOCK AT THE TOP OF THE FILE
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath("com.google.dagger:hilt-android-gradle-plugin:2.51.1")
-    }
-}
 
 // REMOVE THE HILT PLUGIN FROM THIS BLOCK
 plugins {
@@ -18,5 +6,5 @@ plugins {
     id("com.google.gms.google-services") version "4.4.2" apply false
     id("com.android.library") version "8.2.2" apply false
     id("org.jetbrains.kotlin.kapt") version "1.9.23" apply false
-    // id("com.google.dagger.hilt.android") version "2.51.1" apply false // <-- DELETE OR COMMENT OUT THIS LINE
+    id("com.google.dagger.hilt.android") version "2.51.1" apply false // <-- DELETE OR COMMENT OUT THIS LINE
 }
