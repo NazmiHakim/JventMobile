15b793a564eacf6f45b4d40265a6c07a93feed58
NazmiHakim <145305124+NazmiHakim@users.noreply.github.com>
2025-06-27 06:54:07 +0800

    Remove "Test Crash" button from Settings screen
    This commit removes the "Test Crash" button from the Settings screen. This button was previously used for testing purposes to intentionally cause a runtime exception.

--- app/src/main/java/com/example/jvent/screen/Settings.kt
+++ app/src/main/java/com/example/jvent/screen/Settings.kt
@@ -67,18 +67,6 @@ fun Settings(
                     }
                 }
             }
-
-            item {
-                // Tombol untuk memaksa crash (HANYA UNTUK PENGUJIAN)
-                Button(
-                    onClick = {
-                        throw RuntimeException("Test Crash") // Baris ini akan menyebabkan crash
-                    },
-                    modifier = Modifier.fillMaxWidth()
-                ) {
-                    Text("Test Crash")
-                }
-            }
         }
     }
 }
